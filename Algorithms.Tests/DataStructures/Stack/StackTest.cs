917b7d673b9ae37d856409304b75d90f5d8d8a99
Akash Chakrabarti <chkaksh22@gmail.com>
2020-04-25 19:40:36 +0530

    Added code and unit tests for the Stack data structure.

---
+++ Algorithms.Tests/DataStructures/Stack/StackTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+using Xunit;
+using Algorithms.DataStructures.Stack;
+
+namespace Algorithms.Tests.DataStructures.Stack
+{
+    public class StackTest
+    {
+        private Stack<int> _stack;
+
+        public StackTest()
+        {
+            _stack = new Stack<int>();
+        }
+
+        [Fact]
+        public void IsEmpty_EmptyStackShouldWork()
+        {
+            Assert.True(_stack.IsEmpty);
+            Assert.Equal(0, _stack.Count);
+        }
+
+        [Fact]
+        public void IsEmpty_NonEmptyStackShouldWork()
+        {
+            _stack.Push(0);
+            Assert.False(_stack.IsEmpty);
+            Assert.Equal(1, _stack.Count);
+        }
+
+        [Fact]
+        public void Pop_EmptyStackShouldFail()
+        {
+            Assert.Throws<Exception>(() => _stack.Pop());
+        }
+
+        [Fact]
+        public void Peek_EmptyStackShouldFail()
+        {
+            Assert.Throws<Exception>(() => _stack.Peek());
+        }
+
+        [Fact]
+        public void Push_AnyStackShouldWork()
+        {
+            Assert.True(_stack.IsEmpty);
+            _stack.Push(1);
+            Assert.Equal(1, _stack.Count);
+
+            Assert.False(_stack.IsEmpty);
+            _stack.Push(3);
+            Assert.Equal(2, _stack.Count);
+        }
+
+        [Fact]
+        public void Pop_NonEmptyStackShouldWork()
+        {
+            _stack.Push(10);
+            _stack.Push(20);
+            _stack.Push(30);
+
+            Assert.Equal(3, _stack.Count);
+            Assert.Equal(30, _stack.Pop());
+            Assert.Equal(2, _stack.Count);
+        }
+
+        [Fact]
+        public void Peek_NonEmptyStackShouldWork()
+        {
+            _stack.Push(10);
+            _stack.Push(20);
+            _stack.Push(30);
+
+            Assert.Equal(3, _stack.Count);
+            Assert.Equal(30, _stack.Peek());
+            Assert.Equal(3, _stack.Count);
+        }
+    }
+}
---
+++ Algorithms/DataStructures/Stack/Stack.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.DataStructures.Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private LinkedList<T> _list = new LinkedList<T>();
+
+        // Create an empty stack
+        public Stack() { }
+
+        // Create a stack with an initial element
+        public Stack(T firstElem)
+        {
+            Push(firstElem);
+        }
+
+        // Return the number of elements in the stack
+        public int Count { get => _list.Count; }
+
+        // Check if the stack is empty
+        public bool IsEmpty { get => Count == 0;}
+
+        // Push an element on the stack
+        public void Push(T elem)
+        {
+            _list.AddLast(elem);
+        }
+
+        // Pop an element off the stack
+        public T Pop()
+        {
+            if (IsEmpty)
+            {
+                throw new Exception("Stack is empty");
+            }
+            else
+            {
+                T lastElem = _list.Last.Value;
+                _list.RemoveLast();
+                return lastElem;
+            }
+        }
+
+        // Peek the element at the top of the stack without removing it
+        public T Peek()
+        {
+            if (IsEmpty)
+            {
+                throw new Exception("Stack is empty");
+            }
+            else
+            {
+                return _list.Last.Value;
+            }
+        }
+
+        // Implement the GetEnumerator() method to support foreach iteration
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
